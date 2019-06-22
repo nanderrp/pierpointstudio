8fec9cf8d7e479ef02f54b124534b3b141643b65
apple <apple@Alexanders-MacBook-Pro.local>
2019-06-22 11:05:59 -0700

    Adding Captcha

---
+++ email.js
@@ -0,0 +1,52 @@
+
+
+function sendMail() {
+  // getName
+  // getBusinessName
+  // getEmail
+  // getType
+ var name = document.getElementById("name").value;
+ var business = document.getElementById("business").value;
+ var email = document.getElementById("email").value;
+ var dropdown = document.getElementById("dropdown").value;
+ var how = document.getElementById("how").value;
+
+
+ var template_params = {
+   "reply_to": email,
+   "from_name": name,
+   "name": name,
+   "business": business,
+   "email": email,
+   "dropdown": dropdown,
+   "how": how
+};
+
+var service_id = "default_service";
+var template_id = "template_1SvvVysN";
+emailjs.send(service_id, template_id, template_params);
+}
+
+function success() {
+
+
+
+
+  //site key 6LeNLqoUAAAAAGOd386OPuSlB8zBSkMdi0UrfTaL
+  //secret key 6LeNLqoUAAAAAFz7HHjHqCAuTdeGNs8JgDKkRKKK
+
+
+}
+
+
+
+
+
+
+
+emailjs.send('<YOUR SERVICE ID>', '<YOUR TEMPLATE ID>', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
