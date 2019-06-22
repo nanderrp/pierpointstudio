48bc1dbffe67053981946fdf632104f22458566b
apple <apple@Alexanders-MacBook-Pro.local>
2019-06-22 09:03:50 -0700

    t commit -m Update Studio git commit -m  "Update Studio"

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 //jshint esversion:6
 
 const express = require("express");
-
-var feed = new Instafeed ({
-  get: 'user',
-  userId: '7634129121',
-  accessToken: "7634129121.1677ed0.4a824195d5fc4bfbba53bb19b57b3bd9",
-  sortBy: "most-recent"
-  });
-  feed.run();
